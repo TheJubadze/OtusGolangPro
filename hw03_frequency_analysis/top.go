c3db9127a582b0ba00ca44fb47ecfbd748b8074e
Vitaly Senokosov <6545415@gmail.com>
2024-04-19 18:09:25 +0300

    HW3 completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,55 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(str string) []string {
+	wordsMap := make(map[string]int)
+	for _, word := range strings.Fields(str) {
+		wordsMap[word]++
+	}
+
+	sorted := rankByWordCount(wordsMap)
+	maxCount := 10
+	ans := []string{}
+
+	for i, pair := range sorted {
+		if i >= maxCount {
+			break
+		}
+		ans = append(ans, pair.Key)
+	}
+
+	return ans
 }
+
+func rankByWordCount(wordFrequencies map[string]int) PairList {
+	pl := make(PairList, len(wordFrequencies))
+	i := 0
+	for k, v := range wordFrequencies {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(pl)
+	return pl
+}
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int { return len(p) }
+func (p PairList) Less(i, j int) bool {
+	if p[i].Value > p[j].Value {
+		return true
+	} else if p[i].Value == p[j].Value {
+		return p[i].Key < p[j].Key
+	}
+	return false
+}
+func (p PairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -43,9 +43,66 @@ var text = `Как видите, он  спускается  по  лестни�
 	посидеть у огня и послушать какую-нибудь интересную сказку.
 		В этот вечер...`
 
+var text2 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Aenean vulputate ipsum nec nunc consequat tincidunt. Donec eget volutpat
+lorem, id blandit lorem. Pellentesque dapibus mattis metus, at semper 
+diam mattis in. Sed tincidunt justo nisi, eget faucibus dolor tristique
+at. Vivamus in viverra risus. Mauris ut turpis sagittis massa pretium 
+efficitur eget non urna. Duis tristique, erat id accumsan blandit, purus 
+diam tristique leo, eget accumsan eros tortor eget diam. Etiam ultrices 
+placerat risus, ut fermentum est lobortis eget. Aliquam semper, enim sit 
+amet dictum rutrum, lacus tellus pharetra lacus, id vehicula diam lacus 
+vitae turpis. Nulla laoreet ultricies vulputate. Pellentesque posuere 
+nulla sed cursus iaculis. Pellentesque tempus porttitor massa a fermentum.
+Integer convallis consectetur convallis. Maecenas egestas ex tincidunt 
+erat vehicula rutrum. Morbi ipsum libero, consequat sed lectus sed, tincidunt
+bibendum augue.
+Duis consequat feugiat orci lobortis eleifend. Curabitur sollicitudin rutrum 
+gravida. Nam efficitur posuere justo sed molestie. Nunc euismod, dui a congue
+posuere, mauris dui egestas mi, eu iaculis risus libero et mi. Aliquam ut 
+convallis justo. Vestibulum vel eros sed nisi porttitor malesuada. Praesent 
+sollicitudin quis nunc at auctor. Cras lectus magna, vehicula a pulvinar vel,
+egestas et metus. Morbi congue tristique leo nec imperdiet. Mauris nisi magna,
+malesuada eget tellus sed, dignissim aliquam justo. Nulla leo nisl, porta 
+in neque a, laoreet sollicitudin nisl. Maecenas nec cursus nisl, sed 
+consequat mauris. Etiam neque eros, tincidunt vehicula auctor quis, tincidunt
+ut neque. Nam lobortis eros nec velit ultricies, vel dignissim magna faucibus.
+Nulla sodales quis ex vel dictum.
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+Praesent ac mauris massa. Nam nibh ligula, pulvinar et elit sit amet, interdum efficitur
+felis. Fusce non diam in turpis efficitur sagittis at sit amet justo. Etiam turpis massa,
+lobortis vel lacus in, vestibulum semper velit. Integer commodo dui elit, sed tincidunt 
+diam consectetur quis. In eu dui in lectus mollis tempor sit amet in felis. Interdum et
+malesuada fames ac ante ipsum primis in faucibus.
+Sed quis pharetra magna. Nam efficitur rutrum augue, eget tincidunt elit venenatis in. Donec 
+vel tincidunt ante, ac dignissim lorem. Sed vitae dui convallis, lobortis tellus vitae, 
+posuere massa. Pellentesque suscipit enim justo, et sodales orci eleifend non. Curabitur nec 
+libero sollicitudin, vulputate diam eget, feugiat felis. Donec eu felis laoreet, tincidunt arcu
+eu, pulvinar orci. Maecenas erat metus, dignissim in ultricies non, aliquam id neque. Proin 
+quis elementum sapien. Praesent scelerisque cursus sem, nec ullamcorper magna eleifend in.
+Donec auctor pulvinar tempus. Sed turpis lacus, mollis sed odio ut, faucibus semper lectus.
+Quisque vel tempor odio, eget tristique eros. Donec venenatis ut metus vel tincidunt. Curabitur
+molestie tristique finibus.
+Vestibulum magna leo, mollis quis tortor nec, rhoncus hendrerit dui. Orci varius natoque penatibus
+et magnis dis parturient montes, nascetur ridiculous mus. Nullam vel interdum nulla, et posuere orci.
+Curabitur accumsan elit posuere dui facilisis, eu tristique turpis aliquam. Suspendisse sed mi urna.
+Nam aliquam sapien mi, vitae laoreet felis facilisis ut. Vestibulum accumsan ipsum non urna elementum
+vestibulum. In metus est, accumsan nec leo id, commodo laoreet libero. Cras sodales, tellus nec 
+ultrices vulputate, erat felis congue lorem, eget suscipit magna nisl et tellus. Nunc egestas turpis
+sit amet porta feugiat. Etiam lobortis diam ac metus laoreet mollis. Mauris sit amet faucibus sapien,
+at facilisis enim. Pellentesque erat dui, luctus sed turpis eget, finibus condimentum est.`
+
 func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
 		require.Len(t, Top10(""), 0)
+	})
+
+	t.Run("case sensitivity", func(t *testing.T) {
+		require.Len(t, Top10("Нога нога"), 2)
+	})
+
+	t.Run("punctuation sensitivity", func(t *testing.T) {
+		require.Len(t, Top10("нога, нога - ---"), 4)
 	})
 
 	t.Run("positive test", func(t *testing.T) {
@@ -77,6 +134,20 @@ func TestTop10(t *testing.T) {
 				"то",        // 4
 			}
 			require.Equal(t, expected, Top10(text))
+
+			expected = []string{
+				"eget",      // 9
+				"et",        // 9
+				"sed",       // 9
+				"tincidunt", // 9
+				"in",        // 8
+				"nec",       // 8
+				"vel",       // 8
+				"diam",      // 7
+				"sit",       // 7
+				"turpis",    // 7
+			}
+			require.Equal(t, expected, Top10(text2))
 		}
 	})
 }
