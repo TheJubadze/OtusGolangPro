c19d7cf92be0514713f2ade6588176b9d0ab8fcd
Vitaly Senokosov <6545415@gmail.com>
2024-04-24 10:30:31 +0300

    implement list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,10 @@
 package hw04lrucache
 
+import (
+	"strconv"
+	"strings"
+)
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -17,10 +22,89 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len   int
+	front *ListItem
+	back  *ListItem
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newNode := new(ListItem)
+	newNode.Value = v
+	newNode.Next = l.front
+	if l.front != nil {
+		l.front.Prev = newNode
+	}
+	if l.back == nil {
+		l.back = newNode
+	}
+	l.front = newNode
+	l.len++
+	return newNode
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newNode := new(ListItem)
+	newNode.Value = v
+	newNode.Prev = l.back
+	if l.back != nil {
+		l.back.Next = newNode
+	}
+	if l.front == nil {
+		l.front = newNode
+	}
+	l.back = newNode
+	l.len++
+	return newNode
+}
+
+func (l *list) Remove(i *ListItem) {
+	prev := i.Prev
+	next := i.Next
+	prev.Next = next
+	next.Prev = prev
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if l.front == i {
+		return
+	}
+	if l.back == i {
+		l.back = l.back.Prev
+	}
+	prev := i.Prev
+	next := i.Next
+	prev.Next = next
+	if next != nil {
+		next.Prev = prev
+	}
+	i.Prev = nil
+	i.Next = l.front
+	l.front.Prev = i
+	l.front = i
+}
+
+func (l *list) String() string {
+	arr := make([]string, l.len)
+	node := l.front
+	for i := 0; node != nil; i, node = i+1, node.Next {
+		arr[i] = strconv.Itoa((node.Value.(int)))
+	}
+	return "[" + strings.Join(arr, ", ") + "]"
 }
