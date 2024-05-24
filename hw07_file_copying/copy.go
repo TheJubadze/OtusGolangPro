c9742bb2beefc52b67b4af1601e6e5c990f6a6ad
Vitaly Senokosov <6545415@gmail.com>
2024-05-24 09:47:42 +0300

    HW7 completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +15,68 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	srcFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer closeFile(srcFile)
+
+	fi, err := srcFile.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fi.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	dstFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer closeFile(dstFile)
+
+	if limit == 0 || limit > fi.Size() {
+		limit = fi.Size()
+	}
+	if limit+offset > fi.Size() {
+		limit = fi.Size() - offset
+	}
+
+	err = copyFile(srcFile, dstFile, offset, limit)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func copyFile(srcFile *os.File, dstFile *os.File, offset int64, limit int64) error {
+	if limit <= 0 {
+		return nil
+	}
+
+	_, err := srcFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	bar := pb.Full.Start64(limit)
+	reader := bar.NewProxyReader(io.LimitReader(srcFile, limit))
+
+	_, err = io.Copy(dstFile, reader)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
+
+	bar.Finish()
+
+	return nil
+}
+
+func closeFile(file *os.File) {
+	err := file.Close()
+	if err != nil {
+		log.Panic(err)
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,58 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		offset int64
+		limit  int64
+		err    error
+	}{
+		{0, 0, nil},
+		{0, 10, nil},
+		{0, 1000, nil},
+		{0, 10000, nil},
+		{100, 1000, nil},
+		{6000, 1000, nil},
+		{60000, 1000, ErrOffsetExceedsFileSize},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			testCopy(t, tt.offset, tt.limit, tt.err)
+		})
+	}
+}
+
+func testCopy(t *testing.T, offset, limit int64, expectedErr error) {
+	t.Helper()
+	const fromName = "./testdata/input.txt"
+	to, err := os.CreateTemp(".", "tmp.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := to.Close(); err != nil {
+			t.Fatal(err)
+		}
+		if err := os.Remove(to.Name()); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	expectedFilePath := fmt.Sprintf("./testdata/out_offset%d_limit%d.txt", offset, limit)
+
+	err = Copy(fromName, to.Name(), offset, limit)
+
+	f1, err1 := os.ReadFile(to.Name())
+	f2, err2 := os.ReadFile(expectedFilePath)
+	require.Equal(t, expectedErr, err, "copy failed")
+	require.Nil(t, err1, "actual file read failed")
+	require.Nil(t, err2, "expected file read failed")
+	require.True(t, bytes.Equal(f1, f2), fmt.Sprintf("expected and actual files are not equal: %s", expectedFilePath))
 }
