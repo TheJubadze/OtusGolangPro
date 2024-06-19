25f9df2f2ab60abb4f6ddf7c73d09bd0e9cab9ad
Vitaly Senokosov <6545415@gmail.com>
2024-06-19 20:00:07 +0300

    HW10 completed

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,11 +1,13 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
 	"io"
-	"regexp"
 	"strings"
+	"unicode"
+
+	"github.com/goccy/go-json"
 )
 
 type User struct {
@@ -31,36 +33,49 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 type users [100_000]User
 
 func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
+	scanner := bufio.NewScanner(r)
+	for i := 0; scanner.Scan(); i++ {
+		if err = json.Unmarshal(scanner.Bytes(), &result[i]); err != nil {
 			return
 		}
-		result[i] = user
+	}
+
+	if err = scanner.Err(); err != nil {
+		return
 	}
 	return
 }
 
 func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
+	domain = "." + domain
 
 	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
+		email := user.Email
+		atIndex := strings.LastIndex(email, "@")
+		if atIndex == -1 {
+			continue
 		}
+		emailDomain := email[atIndex+1:]
 
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		if hasSuffix(emailDomain, domain) {
+			lowerEmailDomain := strings.ToLower(emailDomain)
+			result[lowerEmailDomain]++
 		}
 	}
 	return result, nil
+}
+
+func hasSuffix(s, suffix string) bool {
+	sLen := len(s)
+	suffixLen := len(suffix)
+	if suffixLen > sLen {
+		return false
+	}
+	for i := 0; i < suffixLen; i++ {
+		if unicode.ToLower(rune(s[sLen-suffixLen+i])) != unicode.ToLower(rune(suffix[i])) {
+			return false
+		}
+	}
+	return true
 }
